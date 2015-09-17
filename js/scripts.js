41f437659a23444e17dbe1c4f8da1675b1db41bc
Molly Waggett & perry eising <student@epicodus.com>
2015-09-17 10:24:07 -0700

    Write functions for creating deck of cards; specs met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,50 @@
-// var functionName = function(params) {
-//
-// };
+var cardValues = function() {
 
-// $(document).ready(function() {
-//
-// });
+  var cardValues = new Map();
+
+  cardValues.set("Ace", 11);
+  cardValues.set(2, 2);
+  cardValues.set(3, 3);
+  cardValues.set(4, 4);
+  cardValues.set(5, 5);
+  cardValues.set(6, 6);
+  cardValues.set(7, 7);
+  cardValues.set(8, 8);
+  cardValues.set(9, 9);
+  cardValues.set(10, 10);
+  cardValues.set("Jack", 10);
+  cardValues.set("Queen", 10);
+  cardValues.set("King", 10);
+  return cardValues;
+}
+
+var createSuits = function() {
+  var suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+  return suits;
+}
+
+var createValues = function() {
+  var values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+  return values;
+}
+
+var createDeck = function() {
+  debugger;
+  var suits = createSuits();
+  var values = createValues();
+  var deck = [];
+
+  for (var suit in suits) {
+    for (var value in values) {
+      deck.push([values[value], suits[suit]]);
+    }
+  }
+  return deck;
+}
+
+
+
+
+$(document).ready(function() {
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,33 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('cardValues', function() {
+  it("is a map", function() {
+    expect(cardValues()).to.be.instanceOf(Map);
+  });
+
+  it("creates key/value pairs of card numbers and their blackjack values", function() {
+    expect(cardValues().get("King")).to.equal(10);
+  });
+});
+
+describe('createSuits', function() {
+  it("creates array of suits", function() {
+    expect(createSuits()).to.be.instanceOf(Array);
+  });
+
+  it("contains suits", function() {
+    expect(createSuits()).to.contain("Hearts");
+  });
+});
+
+describe('createDeck', function() {
+  it("creates deck of 52 cards", function() {
+    expect(createDeck().length).to.equal(52);
+  });
+
+  it("creates array of cards", function() {
+    expect(createDeck()).to.be.instanceOf(Array);
+  });
+
+  it("contains array of card value map and suit string", function() {
+    expect(createDeck()[0]).to.eql(["Ace", "Clubs"]);
+  });
+});
